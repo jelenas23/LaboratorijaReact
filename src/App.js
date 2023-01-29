9d87cceb20907e9d7652b80772ca4f7e6c42c5ac
jelenas23 <js20190188@student.fon.bg.ac.rs>
2023-01-29 16:51:14 +0100

    NavBar u root komponenti + instalacija react icons

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './components/HomePage';
 import KontaktPage from './components/KontaktPage';
+import NavBar from './components/NavBar';
 
 function App() {
   
   return(
   <div className="main-div">
     <Router>
+    <NavBar/>
       <Routes>
         <Route path="/" element={<HomePage/ >} />
         <Route path="/kontakt" element={<KontaktPage/ >} />
