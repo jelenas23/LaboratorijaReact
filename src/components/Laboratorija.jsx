3ef15cda95f91f103a8cb49aa3c38483e6749e62
jelenas23 <js20190188@student.fon.bg.ac.rs>
2023-01-29 10:45:48 +0100

    Izvrseno rutiranje ka HomePage.jsx, kreirane dve reuseable Laboratorije.jsx i Laboratorija.jsx

---
+++ src/components/Laboratorija.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Laboratorija() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Laboratorija
---
+++ src/components/Laboratorije.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Laboratorije() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Laboratorije
