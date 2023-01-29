a575e2f5d2bd03d7afa1cfc3fa5aae437e3c88cb
jelenas23 <js20190188@student.fon.bg.ac.rs>
2023-01-29 12:57:33 +0100

    CSS za pocetnu stranu

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,7 +4,10 @@ import Laboratorije from './Laboratorije'
 function HomePage() {
   return (
     <div className='home-main-div'>
-      <h1>hhjhhj</h1>
+      
+      <img className='slika-home' src='https://png2.cleanpng.com/sh/567a4ae0f708e8138959625f1446a4fd/L0KzQYq3U8I2N5lriJH0aYP2gLBuTfxiapD3eeZ4comwg7Twhf5kbV5uhtZAc4T1iX76gBhwd50ye9pubXn2hMPCTfxwa5JxRd5ybnWwfL3qTfNwdaFmhus2aHBwdX68g8k6OpU2T6ZqMXW8RnA3VMA6P2Y8TKMANUO4RIO7UsM0OGQ7RuJ3Zx==/kisspng-laboratory-science-industry-school-chemistry-local-line-llc-company-home-5c992d174a1e96.0409757415535424233036.png'
+       height={250} alt="Naslov slika"></img> 
+       <h1 id="naslov-home" >LAB IS</h1>
       <Laboratorije/>
     </div>
   )
